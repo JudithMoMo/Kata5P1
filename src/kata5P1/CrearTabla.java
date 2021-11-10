36c25b4ddb84b24fe341e762518c9eced1bfb7df
jmm <jmm@LAPTOP-RT9BBE5H>
2021-11-10 10:10:18 +0000

    Kata 5P1 version 2

---
+++ src/kata5P1/CrearTabla.java
@@ -0,0 +1,27 @@
+package kata5P1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CrearTabla {
+
+    public static void CrearTabla() {
+// Cadena de conexión SQLite
+        String url = "jdbc:sqlite:email.db";
+// Instrucción SQL para crear nueva tabla
+        String sql = "CREATE TABLE IF NOT EXISTS direcc_email (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " direccion text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+// Se crea la nueva tabla
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- src/kata5P1/Kata5P1.java
+++ src/kata5P1/Kata5P1.java
@@ -5,11 +5,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import static kata5P1.CrearTabla.CrearTabla;
 
 public class Kata5P1 {
 
     public static void main(String[] args) {
         SelectApp app = new SelectApp();
         app.selectAll();
+        CrearTabla();
     }
 }
